7a939a45736010089953709816b58d857be53946
YancyQi2002 <xiaoyixiu2002@qq.com>
2021-11-12 22:44:50 +0800

    开发环境准备

---
+++ three-learner-demo1/src/App.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import {defineComponent} from "vue";
+
+  export default defineComponent({
+    setup() {},
+  })
+</script>
+
+<template>
+  Hello three-learner
+</template>
+
+<style>
+
+</style>
